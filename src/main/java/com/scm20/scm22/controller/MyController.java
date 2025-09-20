aadf7458a1a756b5b7714655f2c8b44b13cf9909
Rushikesh <rushikeshlohar@3240.com>
2025-09-20 18:47:58 +0530

    creating basic home, service and about page

--- src/main/java/com/scm20/scm22/controller/MyController.java
+++ src/main/java/com/scm20/scm22/controller/MyController.java
@@ -1,6 +1,7 @@
 package com.scm20.scm22.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,5 +10,16 @@ public class MyController {
     @RequestMapping("/home")
     public String homeController(){
         return "home";
+    }
+
+    @RequestMapping("/service")
+    public String service(){
+        return "service";
+    }
+
+    @RequestMapping("/about")
+    public String about(Model model){
+        model.addAttribute("isLogin", true);
+        return "about";
     }
 }
